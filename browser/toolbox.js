4c353ecc697abd1a93cc80e2886264ec70e5440f
James Halliday <mail@substack.net>
2014-03-06 01:21:28 -0800

    button modes

--- browser/toolbox.js
+++ browser/toolbox.js
@@ -1,12 +1,32 @@
 var fs = require('fs');
 var html = fs.readFileSync(__dirname + '/toolbox.html', 'utf8');
 var domify = require('domify');
+var classList = require('class-list');
 
 module.exports = Toolbox;
 
 function Toolbox () {
+    var self = this;
     if (!(this instanceof Toolbox)) return new Toolbox;
-    this.element = domify(html);
+    var div = this.element = domify(html);
+    var buttons = div.querySelectorAll('button');
+    var selected = {};
+    
+    for (var i = 0; i < buttons.length; i++) (function (button) {
+        var mode = button.getAttribute('x-mode');
+        var name = button.getAttribute('name');
+        
+        button.addEventListener('click', function () {
+            if (mode && selected[mode]) selected[mode].remove('selected');
+            if (mode) {
+                selected[mode] = classList(button);
+                selected[mode].add('selected');
+            }
+            
+            self.emit('click', button, name);
+            self.emit(name, button);
+        });
+    })(buttons[i]);
 }
 
 Toolbox.prototype.appendTo = function (target) {
