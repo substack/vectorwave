ee328085c463818ff8c382058a13b2d95dce78b5
James Halliday <mail@substack.net>
2014-03-07 12:07:57 -0800

    move the css around

--- browser/time/format.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (s) {
-    var parts = [ Math.floor(s / 60 % 60), Math.floor(s % 60) ].map(pad);
-    if (s > 60 * 60) parts.unshift(Math.floor(s / 60 / 60));
-    return parts.join(':');
-};
-function pad (n) { return n < 10 ? '0' + n : String(n) }
--- browser/time/parse.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (str) { 
-    var seconds = 0;
-    var parts = str.split(':');
-    for (var i = 0, len = parts.length; i < len; i++) {
-        seconds += Number(parts[i]) * Math.pow(60, len - i - 1);
-    }
-    return seconds;
-};
