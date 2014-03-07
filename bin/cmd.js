c50ba5ea982000f4a59cdfcee76b8600371e2762
James Halliday <mail@substack.net>
2014-03-07 09:50:56 -0800

    bin script

---
+++ bin/cmd.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var http = require('http');
+var ecstatic = require('ecstatic');
+var minimist = require('minimist');
+
+var argv = minimist(process.argv.slice(2), {
+    alias: { p: 'port' },
+    default: { port: 0 }
+});
+var staticd = ecstatic(__dirname + '/static');
+
+var server = http.createServer(function (req, res) {
+    staticd(req, res);
+});
+server.listen(argv.port);
+
+server.on('listening', function () {
+    var port = server.address().port;
+    console.log('listening on http://localhost:' + port);
+});
